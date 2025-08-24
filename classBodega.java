d807b8b80d09a3ce93948fa7890f0c791ced4288
kpinto4 <kpinto4@udi.edu.co>
2025-08-23 20:51:03 -0500

    clase proveedor y bodega

---
+++ classBodega.java
@@ -0,0 +1,12 @@
+public class Bodega {
+    private int idBodega;
+    private String ubicacion;
+    private String nombre;
+
+    public Bodega() {}
+
+    public Bodega(int idBodega, String ubicacion, String nombre) {
+        this.idBodega = idBodega;
+        this.ubicacion = ubicacion;
+        this.nombre = nombre;
+    }
